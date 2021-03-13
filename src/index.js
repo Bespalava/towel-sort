0084f73926ee5d19c9ef7166ce676f3cf07ca0c9
ybespalava <ybespalava@gmail.com>
2021-03-13 20:39:19 +0300

    feat: add function content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if(matrix == undefined || matrix == null || matrix.length == 0){
+      return []
+    }
+
+    return matrix.map((el, index) => {
+    if (index % 2 != 0) {
+        return el.reverse();
+      } else {
+        return el;
+      }
+    }).flat();
 }
